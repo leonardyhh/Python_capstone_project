3df63e4bc64221570f80852b758003fcfe03756f
leonardyhh <leonardyhoward@gmail.com>
2020-01-31 20:45:02 +0700

    second upload

--- app.py
+++ app.py
@@ -1,17 +1,63 @@
 from flask import Flask, request 
+import pandas as pd 
+import sqlite3
 app = Flask(__name__) 
 
-@app.route('/json', methods=['POST']) 
-def json_exmp():
-    
-    req = request.get_json(force=True) # melakukan parsing data json, menyimpannya sebagai dictionary
-    
-    name = req['name']
-    age = req['age']
-    address = req['address']
-    
-    return (f'''Hello {name}, your age is {age}, and your address in {address}
-            ''')
+# mendapatkan keseluruhan data dari <data_name>
+@app.route('/data/get/<data_name>', methods=['GET']) 
+def get_data(data_name): 
+    conn = sqlite3.connect("data/chinook.db")
+    data = pd.read_sql_query("SELECT firstname, lastname, email, company, \
+                                 invoiceid, invoicedate, billingcountry, total \
+                                 FROM invoices \
+                                 left join customers \
+                                 on invoices.customerId = customers.customerId\
+                                 WHERE Email LIKE '%@apple%'", conn)
+    return (data.to_json())
+
+@app.route('/data/get/artist/<data_name>', methods=['GET']) 
+def get_data2(data_name): 
+    conn = sqlite3.connect("data/chinook.db")
+    data = pd.read_sql_query("SELECT tracks.*, albums.Title, genres.Name AS GenreName,\
+                            artists.Name AS ArtistName \
+                            FROM tracks \
+                            LEFT JOIN albums \
+                            ON albums.AlbumId=tracks.AlbumId \
+                            LEFT JOIN genres \
+                            ON genres.GenreId=tracks.GenreId \
+                            LEFT JOIN artists \
+                            ON artists.ArtistId=albums.ArtistId ", conn, index_col='TrackId')
+    return (data.to_json())
+
+@app.route('/data/get/genre/<data_name>/<value>', methods=['GET']) 
+def get_genre(data_name, value): 
+    conn = sqlite3.connect("data/chinook.db")
+    data = pd.read_sql_query("SELECT tracks.*, albums.Title, genres.Name AS GenreName,\
+                            artists.Name AS ArtistName \
+                            FROM tracks \
+                            LEFT JOIN albums \
+                            ON albums.AlbumId=tracks.AlbumId \
+                            LEFT JOIN genres \
+                            ON genres.GenreId=tracks.GenreId \
+                            LEFT JOIN artists \
+                            ON artists.ArtistId=albums.ArtistId ", conn, index_col='TrackId')
+
+    mask = data['GenreName'] == value
+    data = data[mask]
+    return (data.to_json())
+
+# mendapatkan data dengan filter nilai <value> pada kolom <column>
+@app.route('/data/get/equal/<data_name>/<column>/<value>', methods=['GET']) 
+def get_data_equal(data_name, column, value): 
+    conn = sqlite3.connect("data/chinook.db")
+    data = pd.read_sql_query("SELECT c.Country, c.FirstName, c.LastName, invoices.Total, \
+                           invoices.InvoiceDate \
+                           FROM customers as c \
+                           LEFT JOIN invoices \
+                           ON c.CustomerId = invoices.CustomerId",conn)
+    mask = data[column] == value
+    data = data[mask]
+    return (data.to_json())
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000) #run app in debug mode on port 5000
+    app.run(debug=True, port=5000) 
